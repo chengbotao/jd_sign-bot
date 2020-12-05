ec6be5699541e39795b2f5adb020a7cf9656d8a0
chengbotao <chengbotao5221@163.com>
2020-12-06 04:57:21 +0800

    update appjs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  * @Author: Chengbotao
  * @Date: 2020-11-14 11:17:44
  * @LastEditors: Chengbotao
- * @LastEditTime: 2020-12-06 04:45:15
+ * @LastEditTime: 2020-12-06 04:57:10
  * @FilePath: \jd_sign_bot\app.js
  */
 
@@ -71,12 +71,13 @@ async function sendNotify(name, msg, accessToken) {
     json: true
   }
   let res = await rp(options);
-  let tempRes = JSON.parse(res);
-  // 企业微信自建应用推送 access_token 失效，重新发送
-  if (tempRes.errcode === 40014) {
-    console.log("重新发送")
-    start(name);
-  }
+  console.log("企业微信推送结果",res);
+  // let tempRes = JSON.parse(res);
+  // // 企业微信自建应用推送 access_token 失效，重新发送
+  // if (tempRes.errcode === 40014) {
+  //   console.log("重新发送")
+  //   start(name);
+  // }
 }
 
 async function sendRequest(userName, jdCookie, accessToken) {
