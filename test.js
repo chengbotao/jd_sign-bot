f8ed345c0f5bc34a0a4ae05c4efa32976770f927
chengbotao <chengbotao5221@163.com>
2021-04-16 08:44:10 +0800

    update test

--- test.js
+++ test.js
@@ -18,9 +18,9 @@ const corpid = process.env.CORP_ID, // 企业微信ID
 
 // 京东： [企业微信员工ID ： 京东的登录cookie]
 const JDUsers = {
-  "ChengBoTao": process.env.JD_COOKIE,
+  // "ChengBoTao": process.env.JD_COOKIE,
   // "MaXiaoTian": process.env.JD_COOKIE_2,
-  // "HuangMengXin": process.env.HUANGMX
+  "HuangMengXin": process.env.HUANGMX
 }
 
 async function downFile() {
